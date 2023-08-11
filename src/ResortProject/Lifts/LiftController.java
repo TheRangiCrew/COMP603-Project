b2489020c2fbd32b70b7364e7ae5a3273717a789
TheRangiCrew <ryan.z.adam@gmail.com>
2023-08-11 14:36:15 +1200

    Added toString and used new XMLFile helper funtion

--- src/ResortProject/Lifts/LiftController.java
+++ src/ResortProject/Lifts/LiftController.java
@@ -15,7 +15,7 @@ public class LiftController {
     // Constructor
     public LiftController() {
         // Open and get "root" element of the specified XML file
-        XMLFile inputFile = new XMLFile("Lift.xml");
+        XMLFile inputFile = new XMLFile("./resources/Lift.xml");
         Element root = inputFile.root;
         
         // Extract all the Lift elements as a list from the root element
@@ -27,14 +27,15 @@ public class LiftController {
             Element element = (Element) liftList.item(i);
             
             // Parsers
-            String id = element.getAttribute("id");
-            int length = Integer.parseInt(element.getAttribute("length"));
-            int capacity = Integer.parseInt(element.getAttribute("capacity"));
-            String type = element.getAttribute("type");
-            String name = element.getAttribute("name");
-            String openingTime = element.getAttribute("openingTime");
-            String closingTime = element.getAttribute("closingTime");
-            String status = element.getAttribute("status");
+            String id = XMLFile.getTextContent(element, "id");
+            System.out.println(id);
+            int length = Integer.parseInt(XMLFile.getTextContent(element, "length"));
+            int capacity = Integer.parseInt(XMLFile.getTextContent(element, "capacity"));
+            String type = XMLFile.getTextContent(element, "type");
+            String name = XMLFile.getTextContent(element, "name");
+            String openingTime = XMLFile.getTextContent(element, "openingTime");
+            String closingTime = XMLFile.getTextContent(element, "closingTime");
+            String status = XMLFile.getTextContent(element, "status");
             
             // Add Lift to lifts HashSet
             lifts.add(new Lift(id, length, capacity, type, name, openingTime, closingTime, status));
@@ -44,5 +45,16 @@ public class LiftController {
     
     public HashSet<Lift> getLifts() {
         return this.lifts;
+    }
+    
+    @Override
+    public String toString() {
+        String output = "";
+        
+        for (Object lift: lifts) {
+            output += lift.toString() + "\n";
+        }
+        
+        return output;
     }
  }
