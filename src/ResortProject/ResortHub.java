f11d1661b003d88c9a7ea9e372723b7eb8cec945
Robertso95 <Sr95@xtra.co.nz>
2023-08-21 12:38:53 +1200

    init

--- src/ResortProject/ResortHub.java
+++ src/ResortProject/ResortHub.java
@@ -3,6 +3,8 @@ package ResortProject;
 import ResortProject.Data.GlobalData;
 import ResortProject.Menus.MountainCardMenu;
 import ResortProject.Menus.LiftsMenu;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -15,21 +17,38 @@ public class ResortHub {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         boolean quit = false;
+        String EmailLogin;
+        LocalDate DOBLogin;
         while (quit != true) {
             System.out.println("+-----------------------------+");
             System.out.println("| WELCOME TO THE SNOW RESORT! |");
             System.out.println("+-----------------------------+\n");
-
+            String response = null;
+            while (response == null) {
+                try {
+                    System.out.println("Please login.");
+                    System.out.println("Email: ");
+                    response = scan.nextLine();
+                    scan.reset();
+                    System.out.println("Birth date (YYYY-MM-DD): ");
+                    DOBLogin = LocalDate.parse(scan.nextLine().trim());
+                    scan.reset();
+                } catch (DateTimeParseException e) {
+                    System.out.println("Incorrect input, please try again.");
+                    response = null;
+                }
+            }
             while (quit != true) {
-
+                
                 System.out.println("Please choose your desired location.");
                 System.out.println("1. Mountain card");
                 System.out.println("2. Mountain cafe");
                 System.out.println("3. Rental equipment");
-                System.out.println("4. Ski Lifts");
-                System.out.println("Q. Close the program");
-
-                String response = null;
+                System.out.println("4. View ski lift status");
+                System.out.println("L. Log out");
+                System.out.println("Q. Log out and close the program");
+                
+                response = null;
                 while (response == null) {
                     response = scan.nextLine().toLowerCase();
                     scan.reset();
@@ -50,6 +69,9 @@ public class ResortHub {
                             break;
                         case "4":
                             LiftsMenu.liftMenu();
+                            break;
+                        case "L":
+                            
                             break;
                         default:
                             System.out.println("Incorrect input, please try again.");
